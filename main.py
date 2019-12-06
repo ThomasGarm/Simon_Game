a57dbdaed638ae4d4a3ff8813e0d14782f3fab62
python <thomas.win@gmail.com>
2019-12-06 15:05:20 +0100

    branch thomas: fonctionnal game

--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 from game import Game
 
 game = Game()
+continuer = True
 
-continuer = True #boolean
-
-game.initialize_player() #call the player name
-game.level_difficulty() #player choose between three level of difficulty
-
-
-while continuer == True:
-    
-        game.launch_sequence()
-        game.comparison() 
-    
+if __name__ == "__main__":
+    game.initialize_player() #call the player name
+    game.level_difficulty() #player choose between three level of difficulty
+while continuer:
+    game.launch_sequence()
+    if game.comparison() == False:
+        end =input("continue?\n y/n?").lower()
+        try:
+            assert end == "n"
+        except AssertionError as a:
+            pass
+        if end == "n":
+                continuer = False
         
         
     
--- sequence.py
+++ sequence.py
@@ -4,7 +4,7 @@ from random import randint
 class Sequence:
     def __init__(self):
         self.number = []
-        self.last_number = None   
+        #self.last_number = None   
         
     def random_sequence(self): #get a random integer
-        self.number.append(randint(0, self.last_number))
+        self.number.append(randint(0, 10))
